818bda2938dedfd86a59980c5c4d02d656be7495
robin <brb1983@hotmail.com>
2019-03-31 23:08:40 +0800

    新增温度趋势图 可输出指定日期的温度图

--- main.py
+++ main.py
@@ -2,26 +2,49 @@
 import data_process as dp
 import temperature_generator as tg
 import sys
+import getopt
+import temp_history, wendu_trend_chart
 
 DEFAULT_CSV = 'data/temp_pb.csv'
 DEFAULT_HTML_TEMPERATURE = 'output/temperature.html'
+DEFAULT_TREND_TEMPERATURE = 'output/temp_line.html'
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-
-    print('数据分析中，请稍后......')
-    date, data = dp.get_percentile_data(DEFAULT_CSV)
+    # args = sys.argv[1:]
+    opts, args = getopt.getopt(sys.argv[1:], "th:", ["trend", "history"])
 
     input_date = None
-    if len(args) > 0:
-        input_date = args[0]
+    print('处理开始，请稍后......')
+    # 返回的参数opts是对应的选项和参数，args暂时不用管
+    if len(opts) > 0:
+        for op, value in opts:
+            if op == '-h' or op == '--history':
+                print('获取历史数据，请稍后......')
+                input_date = value
+                data = temp_history.get_history_data(input_date)
+                if data is None or len(data) == 0:
+                    print("没有获取到指定日期的指数温度，请输入正确的日期")
+                else:
+                    tg.generate(input_date, [round(i, 1) for i in data[1:7]], DEFAULT_HTML_TEMPERATURE)
+                    print('chart生成完毕,请查看:', DEFAULT_HTML_TEMPERATURE)
     else:
-        input_date = date
-        print('没有输入日期, 使用指数最新日期:', date)
+        print('数据分析中，请稍后......')
+        date, data = dp.get_percentile_data(DEFAULT_CSV)
 
-    print('指数最新日期', date)
-    print('指数温度信息', data)
-    print('数据获取完毕，开始生成chart...')
-    tg.generate(input_date, data, DEFAULT_HTML_TEMPERATURE)
-    print('chart生成完毕,请查看:', DEFAULT_HTML_TEMPERATURE)
+        input_date = None
+        if len(args) > 0:
+            input_date = args[0]
+        else:
+            input_date = date
+            print('没有输入日期, 使用指数最新日期:', date)
+
+        print('指数最新日期', date)
+        print('指数温度信息', data)
+        print('数据获取完毕，开始生成chart...')
+        tg.generate(input_date, data, DEFAULT_HTML_TEMPERATURE)
+        print('chart生成完毕,请查看:', DEFAULT_HTML_TEMPERATURE)
+
+        print('生成历史温度趋势，请稍后......')
+        wendu_trend_chart.generate()
+        print('chart生成完毕,请查看:', DEFAULT_TREND_TEMPERATURE)
     sys.exit(0)
---
+++ util/date_utils.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+
+
+def csvDate2lxrDate(start_date):
+    date_arr = start_date.split('/')
+    year = date_arr[0]
+    month = date_arr[1]
+    if len(month) == 1:
+        month = "0" + month
+    day = date_arr[2]
+    if len(day) == 1:
+        day = "0" + day
+    return '-'.join([year, month, day])
+
+
+def lxrDate2csvDate(start_date):
+    date_arr = start_date.split('-')
+    if len(date_arr) != 3:
+        raise Exception("日期格式不正确，请输入正确的日期格式，例如：2019-02-24")
+    year = date_arr[0]
+    month = str(int(date_arr[1]))
+    day = str(int(date_arr[2]))
+    return '/'.join([year, month, day])
